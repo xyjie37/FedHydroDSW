2b06a2d912172ae215949a6a43d0d80bd58374c1
xyjie37 <131571777+xyjie37@users.noreply.github.com>
2024-07-05 22:56:09 +0800

    Update lstm.py

--- models/lstm.py
+++ models/lstm.py
@@ -17,7 +17,7 @@ class LSTM(nn.Module):
         return prediction
 
 
-class BiLSTM(nn.Module):
+'''class BiLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(BiLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
@@ -27,5 +27,19 @@ class BiLSTM(nn.Module):
         self.lstm.flatten_parameters()
         lstm_out, _ = self.lstm(x)
         lstm_out = lstm_out[:, -1, :]
+        prediction = self.fc(lstm_out)
+        return prediction'''
+class BiLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_rate=0.1):
+        super(BiLSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc = nn.Linear(hidden_size * 2, output_size)
+
+    def forward(self, x):
+        self.lstm.flatten_parameters()
+        lstm_out, _ = self.lstm(x)
+        lstm_out = lstm_out[:, -1, :]
+        lstm_out = self.dropout(lstm_out)
         prediction = self.fc(lstm_out)
         return prediction
